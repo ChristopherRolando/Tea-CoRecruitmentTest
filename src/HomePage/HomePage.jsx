89c6ad44603326c33313c961ce2fb31d3b6b6968
ChristopherRolando <realrolando888@gmail.com>
2021-07-12 13:57:05 +0700

    commit project

---
+++ src/HomePage/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import { userService, authenticationService } from '@/services';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentUser: authenticationService.currentUserValue,
+            users: null
+        };
+    }
+
+    componentDidMount() {
+        userService.getAll().then(users => this.setState({ users }));
+    }
+
+    render() {
+        const { currentUser, users } = this.state;
+        return (
+            <div>
+                <h1>Hi {currentUser.firstName}!</h1>
+                <p>You're logged in</p>
+            </div>
+        );
+    }
+}
+
+export { HomePage };
